cf2751cfee98c0749c4d6722e1ef4c8542b95ec7
mro160 <mmarrero160@gmail.com>
2021-07-19 22:49:06 -0400

    added ability to connect using database url

--- db.js
+++ db.js
@@ -4,13 +4,24 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const conn = {
-	user:     process.env.DB_USER,
-  	host:     process.env.DB_HOST,
- 	database: process.env.DB_NAME,
- 	password: process.env.DB_PASSWORD,
-  	port:     process.env.DB_PORT,
-};
+let conn = '';
+
+if ('DATABASE_URL' in process.env){
+	conn =  {
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false
+		}
+	}
+} else {
+	conn = {
+		user:     process.env.DB_USER,
+		  host:     process.env.DB_HOST,
+		 database: process.env.DB_NAME,
+		 password: process.env.DB_PASSWORD,
+		 port:     process.env.DB_PORT,
+	}
+}
 
 const db = pgp(conn);
 
